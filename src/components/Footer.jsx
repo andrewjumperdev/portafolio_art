5bbffc2eb6775bf1d3abbf0e8332a60806a7d5dd
andrewjumperdev <andrew.jumper.ent@gmail.com>
2023-06-30 09:09:09 +0200

    Grid Images

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer class="footer-distributed">
+      <div class="footer-left">
 
-export default Footer
+          <span><img className="footer__logo" src="./logo192.png" alt="Logo" /></span>
+
+        <p class="footer-links">
+
+          <a href="#">Gallery</a>
+
+          <a href="#">Videos</a>
+
+          <a href="#">Contact</a>
+        </p>
+
+        <p class="footer-company-name">Daniel Lynch © 2023</p>
+      </div>
+
+      <div class="footer-center">
+        <div>
+          <i class="fa fa-map-marker"></i>
+          <p>
+            <span>France</span> Paris
+          </p>
+        </div>
+
+        <div>
+          <i class="fa fa-phone"></i>
+          <p>+1.555.555.5555</p>
+        </div>
+
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p>
+            <a href="mailto:support@company.com">danielynch@company.com</a>
+          </p>
+        </div>
+      </div>
+
+      <div class="footer-right">
+        <p class="footer-company-about">
+          <span>About</span>
+          Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce
+          euismod convallis velit, eu auctor lacus vehicula sit amet.
+        </p>
+
+        <div class="footer-icons">
+          <a href="/">
+          <i class="fa-brands fa-instagram"></i>
+          </a>
+          <a href="/">
+          <i class="fa-brands fa-vimeo-v"></i>
+          </a>
+          <a href="/">
+          <i class="fa-brands fa-youtube"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Hearder.jsx
+++ src/components/Hearder.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
+import React from "react";
+import ReactPlayer from "react-player";
 
-const videos = 'https://vimeo.com/241495043'
+const videos = "https://vimeo.com/241495043";
 
 const Hearder = () => {
   return (
-      <div className="container__player">
-          <ReactPlayer className="player" playing muted url={videos} />
+    <div className="container__header">
+      <p className="description">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quasi
+        illum consequuntur sit ipsam hic autem quam provident laborum, tempora
+        beatae ut alias adipisci nemo facere quae accusantium excepturi. Magni
+        iure fugiat cupiditate, aliquid corrupti deserunt totam velit dolor enim
+        consequatur architecto nostrum esse debitis! Illum, similique dolores
+        soluta nesciunt minima atque molestiae repellat saepe eius sapiente et
+        minus explicabo repudiandae quasi natus magni, nisi, architecto cumque
+        nostrum voluptate! Sed eligendi quia commodi labore aperiam optio
+        quidem, doloribus blanditiis provident iusto deserunt non quae eaque ut
+        quis aliquid fugiat excepturi ducimus assumenda sunt animi dolorum ipsum
+        explicabo magnam? Excepturi, corrupti!
+      </p>
+      <div className="player-wrapper">
+        <ReactPlayer
+          className="react-player"
+          controls={true}
+          playing
+          muted
+          url={videos}
+        />
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Hearder
+export default Hearder;
